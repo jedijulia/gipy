9bbb6baece1900b0bf1a8718cb9db35845401a87
Julia Menchavez <jedijulia@users.noreply.github.com>
2015-11-11 21:16:16 +0800

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='gitignorepy',
+    version='0.0.0',
+    description='cli to create gitignore files from gitignore.io',
+    url='https://github.com/jedijulia/gitignorepy',
+    author='Julia Menchavez',
+    author_email='jcmenchavez@gmail.com',
+    license='MIT',
+    packages=['gitignorepy'])
+
