242792b023b8106a30247e884e7a530ccadf2597
Julia Menchavez <jedijulia@users.noreply.github.com>
2015-11-24 23:06:13 +0800

    added docstring to generate method

--- gitignorepy/__init__.py
+++ gitignorepy/__init__.py
@@ -5,6 +5,12 @@ api_url = 'http://www.gitignore.io/api'
 
 
 def generate(*targets, **kwargs):
+    """
+    Requests gitignore.io and returns the generated gitignore contents
+    @param targets      the languages/frameworks that the generated
+                        gitignore is for
+    @return string      the generated gitignore contents
+    """
     target = ','.join(targets)
     url = '{0}/{1}'.format(api_url, target)
     response = requests.get(url)
