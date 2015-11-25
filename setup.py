a2a5f7d45cde2ac9175006aea6c2f3fad2d312ab
Julia Menchavez <jedijulia@users.noreply.github.com>
2015-11-25 20:58:00 +0800

    added more package info to setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from setuptools import setup
 
 setup(
     name='gitignorepy',
-    version='0.0.0',
+    version='0.1.0',
     description='cli to create gitignore files from gitignore.io',
+    keywords='cli gitignore git',
     url='https://github.com/jedijulia/gitignorepy',
     author='Julia Menchavez',
     author_email='jcmenchavez@gmail.com',
@@ -11,4 +12,10 @@ setup(
     packages=['gitignorepy'],
     scripts=['bin/gitignorepy'],
     install_requires=['requests'],
-    setup_requires=['requests'])
+    setup_requires=['requests'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7'])
